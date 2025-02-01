362d10c7882f11607124653448119442fd11bda9
matta <matta@10.0.0.213>
2025-02-01 16:45:38 -0500

    Second time.

--- Spring_2025_/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ Spring_2025_/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -2,8 +2,12 @@ package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
 
+	/**
+	* Matthew Herriman
+	*
+	*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
 
 	}
 
